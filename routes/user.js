e1fa701c7b35799f497494cbc3bb14c8b79f1023
salvo.bonaccorso89 <salvo.bonaccorso89@gmail.com>
2019-05-08 13:01:32 +0200

    create a delete

--- routes/user.js
+++ routes/user.js
@@ -33,25 +33,38 @@ router.get('/:id', function(req, res) {
         res.json(user);
     });
 });
-// API UPDATE PUT/ID
 
+// API update PUT/ID
 router.put('/:id', function(req, res) {
     User.findOne({_id: req.params.id})
         .exec(function(err, user) {
-          if (err) return res.status(500).json({error: err});
-          if (!user) {
+        if (err) return res.status(500).json({error: err});
+        if (!user) {
             return res.status(404).json({message: 'Utente non trovato'});
-          }
-          for (key in req.body) {
+        }
+        for (key in req.body) {
             if (req.body.hasOwnProperty(key)) {
-              user[key] = req.body[key];
+                user[key] = req.body[key];
             }
-          }
-          user.save(function(err) {
+        }
+        user.save(function(err) {
             if (err) return res.status(500).json({error: err});
             res.json(user);
-          });
         });
-  });
-  
+    });
+});
+
+// API delete DELETE/ID
+router.delete('/:id', function(req, res) {
+    User.findOne({_id: req.params.id})
+    .exec(function(err, user) {
+        if (err) return res.status(500).json({error: err});
+        if (!user) return res.status(404).json({message: 'Utente non trovato'});
+        User.remove({_id: req.params.id}, function(err) {
+            if (err) return res.status(500).json({error: err});
+            res.json({message: 'Utente eliminato correttamente'});
+        });
+    });
+});
+
 module.exports = router;
