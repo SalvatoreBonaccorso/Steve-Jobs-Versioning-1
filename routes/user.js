93e5f4bc85013b69969a30d2f429b35348858aac
Badredin Jamjama <bjamjama@gmail.com>
2019-05-08 12:38:31 +0200

    Api Create metodo post

--- routes/user.js
+++ routes/user.js
@@ -10,4 +10,19 @@ router.get('/', function(req, res) {
     });
 });
 
+// API create POST
+router.post('/', [
+    check('name').exists(),
+    check('surname').exists(),
+    check('email').isEmail(),
+    check('age').isInt(),
+  ],
+  function(req, res) {
+    const newUser = new User(req.body);
+    newUser.save(function(err) {
+      if (err) return res.status(500).json({error: err});
+      res.status(201).json(newUser);
+    });
+  });
+
 module.exports = router;
