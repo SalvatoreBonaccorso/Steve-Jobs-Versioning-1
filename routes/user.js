affc45aa31ea8ceccf484995dc89202374b1ceaa
salvo.bonaccorso89 <salvo.bonaccorso89@gmail.com>
2019-05-08 12:54:26 +0200

    API show GET/ID

--- routes/user.js
+++ routes/user.js
@@ -16,13 +16,22 @@ router.post('/', [
     check('surname').exists(),
     check('email').isEmail(),
     check('age').isInt(),
-  ],
-  function(req, res) {
+    ],
+    function(req, res) {
     const newUser = new User(req.body);
     newUser.save(function(err) {
-      if (err) return res.status(500).json({error: err});
-      res.status(201).json(newUser);
+        if (err) return res.status(500).json({error: err});
+        res.status(201).json(newUser);
     });
-  });
+});
+
+// API show GET/ID 
+router.get('/:id', function(req, res) {
+    User.findOne({_id: req.params.id}, function(err, user) {
+        if (err) return res.status(500).json({error: err});
+        if (!user) return res.status(404).json({message: 'Utente non trovato'});
+        res.json(user);
+    });
+});
 
 module.exports = router;
