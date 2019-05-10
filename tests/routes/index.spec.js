32a596b6e59e2f28d9e9421243bfb7b1c94faebd
salvo.bonaccorso89 <salvo.bonaccorso89@gmail.com>
2019-05-10 09:08:16 +0200

    create second  TESTS

--- tests/routes/index.spec.js
+++ tests/routes/index.spec.js
@@ -8,8 +8,17 @@ chai.use(chaiHttp);
 describe('GET /', () => {
     const expectedResponse = {message: 'API Works'};
     it('Test index API payload', async () => {
-      const result = await chai.request(app).get('/');
-      expect(result.body).to.be.deep.equal(expectedResponse);
-      expect(result.status).to.be.equal(200);
+        const result = await chai.request(app).get('/');
+        expect(result.body).to.be.deep.equal(expectedResponse);
+        expect(result.status).to.be.equal(200);
     });
-  });
+    });
+
+describe('404 Route', () => {
+    const expectedResponse = {message: 'Not Found', error: {status: 404}};
+    it('Test 404 route payload', async () => {
+        const result = await chai.request(app).get('/404api');
+        expect(result.body).to.be.deep.equal(expectedResponse);
+        expect(result.status).to.be.equal(404);
+    });
+    });
