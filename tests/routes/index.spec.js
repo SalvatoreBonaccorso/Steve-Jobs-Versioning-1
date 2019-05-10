55b8ee3539f213890f492c0d56b234b60700c285
Badredin Jamjama <bjamjama@gmail.com>
2019-05-10 08:51:24 +0200

    primo test

--- tests/routes/index.spec.js
+++ tests/routes/index.spec.js
@@ -4,3 +4,12 @@ const chai = require('chai');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
+
+describe('GET /', () => {
+    const expectedResponse = {message: 'API Works'};
+    it('Test index API payload', async () => {
+      const result = await chai.request(app).get('/');
+      expect(result.body).to.be.deep.equal(expectedResponse);
+      expect(result.status).to.be.equal(200);
+    });
+  });
